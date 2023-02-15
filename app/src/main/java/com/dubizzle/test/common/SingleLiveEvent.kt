178170ae86f35649c156b295a5c9d7e6af00c37b
Narayan Soni <narayansoni@Narayans-Laptop.local>
2023-02-15 14:49:54 +0530

    udate

--- app/src/main/java/com/dubizzle/test/common/SingleLiveEvent.kt
+++ app/src/main/java/com/dubizzle/test/common/SingleLiveEvent.kt
@@ -16,11 +16,11 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
             Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
         }
         // Observe the internal MutableLiveData
-        super.observe(owner, { t ->
+        super.observe(owner) { t ->
             if (mPending.compareAndSet(true, false)) {
                 observer.onChanged(t)
             }
-        })
+        }
     }
 
     @MainThread
